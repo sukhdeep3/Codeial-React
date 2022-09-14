cbc7bea05ed1d4058daab46f420fb9fc48b66497
sukhdeepsukh589 <sukhdeepsukh589@gmail.com>
2022-09-14 06:21:37 -0400

    fetch the data using api

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,19 @@
+import { useEffect } from "react";
+import { getPosts } from '../api'
 
 
 function App() {
+
+  useEffect(async()=>{
+
+    const fetchPosts = async ()=>{
+      const response = await getPosts();
+      console.log('response', response);
+    }
+    
+    fetchPosts();
+  }, [])
+
   return (
     <div className="App">
       Hello World
