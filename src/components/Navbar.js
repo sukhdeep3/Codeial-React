723326b4fc2a13e277471574f1fd58c7cd71b196
sukhdeepsukh589 <sukhdeepsukh589@gmail.com>
2022-10-06 06:27:43 -0400

    added ui for navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,11 @@
 import styles from '../styles/navbar.module.css';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../hooks';
+import { useState } from 'react';
 
 const Navbar = () => {
+  const [results, setResults] = useState('');
+  const [searchText, setSearchText] = useState(false);
   const auth = useAuth();
   return (
     <div className={styles.nav}>
@@ -13,6 +16,39 @@ const Navbar = () => {
             src="https://ninjasfiles.s3.amazonaws.com/0000000000003454.png"
           />
         </Link>
+      </div>
+      <div className={styles.searchContainer}>
+        <img
+          className={styles.searchIcon}
+          src="https://t3.ftcdn.net/jpg/01/05/18/78/240_F_105187826_f9QmMttoqksCW1abMmDCAv8q2u265Saa.jpg"
+          alt=""
+        />
+        <input
+          placeholder="Search Users"
+          value={results}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+        {results.length > 0 && (
+          <div className={styles.searchResults}>
+            <ul>
+              {results.map((user) => {
+                <li
+                  className="styles.searchResultsRow"
+                  key={`users-${user._id}`}
+                >
+                  <Link to={`/users/${user._id}`}>
+                    <img
+                      src="https://as2.ftcdn.net/v2/jpg/00/65/77/27/1000_F_65772719_A1UV5kLi5nCEWI0BNLLiFaBPEkUbv5Fv.jpg"
+                      alt="UserDp"
+                      className={styles.userDp}
+                    />
+                    <span>{user.name}</span>
+                  </Link>
+                </li>;
+              })}
+            </ul>
+          </div>
+        )}
       </div>
 
       <div className={styles.rightNav}>
