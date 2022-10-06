1e6c0fa070e67e6137b7b569762eec65f394cca5
sukhdeepsukh589 <sukhdeepsukh589@gmail.com>
2022-10-06 05:49:16 -0400

    added likes to comment section

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,8 +1,35 @@
 import PropTypes from 'prop-types';
+import { useToasts } from 'react-toast-notifications';
+import { toggleLike } from '../api';
+import { usePosts } from '../hooks';
 
 import styles from '../styles/home.module.css';
 
 const Comment = ({ comment }) => {
+  const posts = usePosts();
+  const { addToast } = useToasts();
+
+  const handleCommentLikeClick = async () => {
+    console.log('comment', comment);
+    const response = await toggleLike(comment._id, 'Comment');
+
+    if (response.success) {
+      if (response.data.deleted) {
+        addToast('Like removed successfully', {
+          appearance: 'success',
+        });
+      } else {
+        addToast('Like added successfully', {
+          appearance: 'success',
+        });
+      }
+    } else {
+      addToast(response.message, {
+        appearance: 'error',
+      });
+    }
+  };
+
   return (
     <div className={styles.postCommentsItem}>
       <div className={styles.postCommentHeader}>
@@ -11,7 +38,18 @@ const Comment = ({ comment }) => {
         <span className={styles.postCommentLikes}>22</span>
       </div>
 
-      <div className={styles.postCommentContent}>{comment.content}</div>
+      <div className={styles.postActions}>
+        <div className={styles.postCommentContent}>{comment.content}</div>
+        <div className={styles.postLike}>
+          <button onClick={handleCommentLikeClick}>
+            <img
+              src="https://cdn-icons-png.flaticon.com/128/535/535285.png"
+              alt="likes-icon"
+            />
+          </button>
+          <span>{comment.likes.length}</span>
+        </div>
+      </div>
     </div>
   );
 };
